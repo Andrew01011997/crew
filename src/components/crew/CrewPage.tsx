4d1ec3730094b63d6b788a04b4cf6291bf7f94bb
Andrew <andrew01.01.1997@gmail.com>
2019-11-09 17:17:09 +0200

    create auto-assigning function

--- src/components/crew/CrewPage.tsx
+++ src/components/crew/CrewPage.tsx
@@ -1,16 +1,34 @@
 import React, { useState, useEffect } from 'react'
-import { Crew } from '../../emulator/types'
+import { Crew, Job, Member, JobSplit } from '../../emulator/types'
 import crewService from '../../emulator/crewService'
 import CrewTable from './CrewTable'
-import crewManager from '../../emulator/internals/crewManager'
+import settingsService from '../../emulator/settingsService'
 
 function CrewPage(props: {}) {
   const [needRefresh, setNeed] = useState(true)
   const [crew, setCrew] = useState<Crew>([])
+
+  const getJob = (mem : Member, crews: Crew, needJob: JobSplit) =>{
+
+    const needMedic = crews.filter(elem => elem.job === Job.medic).length / needJob.medic
+    const needPilot = crews.filter(elem => elem.job === Job.pilot).length / needJob.pilot
+    const needEngineer = crews.filter(elem => elem.job === Job.engineer).length / needJob.engineer;
+
+    if(needEngineer > needMedic){
+      return needMedic>needPilot? 1:2
+    }
+    else{
+      return needEngineer>needPilot? 1:3
+    }
+
+  }
+
   useEffect(
     () => {
       const unsub = crewService.onMemberAdded(
-        () => {
+        (member) => {
+          const jobType = getJob(member, crew, settingsService.getJobSplit())
+          crewService.assignJob(member.id, jobType==1?Job.pilot:jobType==2?Job.medic:Job.engineer)
           setNeed(true)
         }
       )
